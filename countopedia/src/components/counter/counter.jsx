8e9803026fe8be8a95711a83e48afc5dd63f72fd
Alex Bondarenko <albom2004q@gmail.com>
2023-06-27 10:41:18 +0300

    Started working with main project logic

--- countopedia/src/components/counter/counter.jsx
+++ countopedia/src/components/counter/counter.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 class Counter extends React.Component {
+  handleAttack = () => {
+    alert("Attack clicked");
+  };
+  handleDefence = () => {
+    alert("Defend clicked");
+  };
+
   render() {
     const counter = "Counter";
-    return <h1 style={{ color: "white" }}>{counter}</h1>;
+    return (
+      <div className="row text-center">
+        <h1 style={{ color: "white" }}>{counter}</h1>
+        <div>
+          <button onClick={this.handleAttack} style={{ width: "200px" }}>
+            +1
+          </button>
+          <button onClick={this.handleDefence} style={{ width: "200px" }}>
+            -1
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
