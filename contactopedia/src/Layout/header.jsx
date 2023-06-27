7cc07452de061b314037e6b4227950f5dd53c386
Alex Bondarenko <albom2004q@gmail.com>
2023-06-27 12:49:26 +0300

    [ContactOPedia] Initial commit

---
+++ contactopedia/src/Layout/header.jsx
@@ -0,0 +1,16 @@
+import logo from "../images/react.png";
+const Header = () => {
+  return (
+    <div className="py-2 pt-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+      <img
+        src={logo}
+        style={{ height: "55px", verticalAlign: "top" }}
+        alt="Oops..."
+      ></img>
+      <span className="h2 pt-4 m-2" style={{ color: "lightGray" }}>
+        React Course - ContactOPedia
+      </span>
+    </div>
+  );
+};
+export default Header;
---
+++ contactopedia/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import Header from "./Layout/header";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <Header></Header>
+  </div>
+);
