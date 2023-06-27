61ae8fe1db779543e8e9d749fa0d1d45204983d7
Alex Bondarenko <albom2004q@gmail.com>
2023-06-27 10:50:36 +0300

    [CountOPedia] Proceeding work with program logic

--- countopedia/src/components/counter/counter.jsx
+++ countopedia/src/components/counter/counter.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAttack = this.handleAttack.bind(this);
+    this.handleDefence = this.handleDefence.bind(this);
+    this.state = { count: 0 };
+  }
   handleAttack = () => {
-    alert("Attack clicked");
+    // alert("Attack clicked");
+    this.setState({ count: this.state.count + 1 });
   };
   handleDefence = () => {
-    alert("Defend clicked");
+    //alert("Defend clicked");
+    this.setState({ count: this.state.count - 1 });
   };
 
   render() {
     const counter = "Counter";
     return (
       <div className="row text-center">
-        <h1 style={{ color: "white" }}>{counter}</h1>
+        <h1 style={{ color: "white" }}>
+          {counter} : {this.state.count}
+        </h1>
         <div>
           <button onClick={this.handleAttack} style={{ width: "200px" }}>
             +1
